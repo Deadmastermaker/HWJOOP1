5c79f27286868a9c6a593dd047043cfac38c2277
unknown <artem.antonovski@gmail.com>
2023-03-17 18:36:18 +0300

    first commit

---
+++ Lemonade.java
@@ -0,0 +1,8 @@
+package HWJAVAOOP1;
+
+public class Lemonade extends Drinkables{
+    public Lemonade(String name, Double price, Integer amount, String unit, Double volume) {
+        super(name, price, amount, unit, volume);
+    }
+
+}
---
+++ Nipple.java
@@ -0,0 +1,9 @@
+package HWJAVAOOP1;
+
+public class Nipple extends ChildrenGoods{
+
+    public Nipple(String name, Double price, Integer amount, String unit,
+                  Integer minAge, String hypoallergenic) {
+        super(name, price, amount, unit, minAge, hypoallergenic);
+    }
+}
